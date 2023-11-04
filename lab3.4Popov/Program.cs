0df915cce3492d1372351a9e5cdc573df426b0e7
b.r.o.t.e.m.oo@gmail.com <b.r.o.t.e.m.oo@gmail.com>
2023-11-05 00:49:08 +0200

    Добавьте файлы проекта.

---
+++ lab3.4Popov/Program.cs
@@ -0,0 +1,22 @@
+﻿//высокий 6
+Console.WriteLine("|    x   |   y    |");
+for (double x = -3; x <= 2.5; x += 1.1) 
+{ 
+    for (int k = 1; k <= 8; k++)
+    {
+        {
+            double y;
+            if (x <= 0) y=Math.Tan(Math.Pow(Math.PI,Math.PI));
+            else
+            {
+                double kFact = 1;
+                for (int i = 1; i <= k; i++)
+                {
+                    kFact *= i;
+                }
+                y = Math.Pow(-1, k) * Math.Pow(x, k) / kFact;
+            }
+            Console.WriteLine($"|{x,8:F2}|{y,8:F2}|");
+        }
+    }
+}
